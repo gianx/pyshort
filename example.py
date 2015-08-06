ef6c67110ab070834b484dd67e21ed183e64324d
gianx <gianluca@gianlucanieri.com>
2015-08-06 10:30:01 +0200

    Removed example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from pyshort import pyshort
-myshort = pyshort("sites_archive.json")
-try:
-    print myshort.generate(astring="http://www.gianlucanieri.com",checksite=False)[0]
-except myshort.SiteConnectionError, e:
-    print "beccato!"
